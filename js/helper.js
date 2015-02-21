a449a3405713821ecbcae50ea4ac1fb85998c445
Sam <sg506w@SKCBC2054.emea.att.com>
2015-02-21 20:30:25 +0100

    basic functionality added, partial commits done in svn

--- js/helper.js
+++ js/helper.js
@@ -64,7 +64,7 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
+    var iName = inName(name) || function(){};
     $('#name').html(iName);  
   });
 });
@@ -85,10 +85,8 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.pageX, loc.pageY);
 });
-
-
 
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
@@ -149,10 +147,23 @@ function initializeMap() {
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
+    var lat = placeData.geometry.location.k;  // latitude from the place service
+    var lon = placeData.geometry.location.D;  // longitude from the place service
     var name = placeData.formatted_address;   // name of the place from the place service
     var bounds = window.mapBounds;            // current boundaries of the map window
+ 
+    //////////////////////////////////////////
+ 	// added custom heading based on location
+ 	////////////////////////////////////////// 
+    if (name == 'Tring, Tring, Hertfordshire HP23, UK') {
+    	var description = "My summer job location";		
+    }
+    else if (name == 'Bratislava, Slovakia') {
+    	var description = "My current location";		 	
+    }
+    else  {
+    	var description = "My beloved hometown";		 	
+    }
 
     // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
@@ -165,12 +176,12 @@ function initializeMap() {
     // or hover over a pin on a map. They usually contain more information
     // about a location.
     var infoWindow = new google.maps.InfoWindow({
-      content: name
+      content: "<h4>" + description + ":</h4>Name: " + name + "<BR>Longitude: " + (Math.round(lon * 100) / 100 ) + "<BR>Latitude: " + (Math.round(lat * 100) / 100 ) 
     });
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+		infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -233,11 +244,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+  //  Make sure the map bounds get updated on page resize
+    map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,185 @@
+bio = {
+	
+	"name" : "Samuel",
+	"role" : "web developer",
+	"contacts" : {
+		"mobile": "0907 939 857",
+		"email": "fake@fake.com",
+		"github": "https://github.com/Kakadu123",
+		"twitter": "@myfaketwitter",
+		"location": "Bratislava, Slovakia"
+	}, 
+	"welcomeMessage": "Hey, whats up!", 
+	"skills": ["hardworking", "proactive", "talkative", "innovative"],
+	"biopic": "images/Samuel_photo.png",
+	"display": function () {
+	
+		$("#header").prepend(HTMLheaderRole.replace( "%data%" , bio.role ))
+					.prepend(HTMLheaderName.replace( "%data%" , bio.name ))
+					.append(HTMLbioPic.replace( "%data%" , bio.biopic ))
+					.append(HTMLWelcomeMsg.replace( "%data%" , bio.welcomeMessage ))
+					.append(HTMLskillsStart);
+					
+		for (var skill in bio.skills) {
+			$("#header").append(HTMLskills.replace( "%data%" , bio.skills[skill] ));
+		}			
+		
+		$("#topContacts, #footerContacts").append(HTMLmobile.replace( "%data%" , bio.contacts.mobile ))
+						 .append(HTMLemail.replace( "%data%" , bio.contacts.email))
+						 .append(HTMLgithub.replace( "%data%" , bio.contacts.github))
+						 .append(HTMLtwitter.replace( "%data%" , bio.contacts.twitter ))
+						 .append(HTMLlocation.replace( "%data%" , bio.contacts.location ));
+	} 
+}
+		
+education = {
+	"schools" : [
+		{
+			"name" : "Elementary school",
+			"location" : "Kezmarok, Slovakia",
+			"degree": "N/A",
+			"majors": [ "N/A" ],
+			"dates": 2001,
+			"url": "http://zsfiskk.edupage.sk/"
+		}
+		,
+		{
+			"name" : "Faculty of Law",
+			"location" : "Bratislava, Slovakia",
+			"degree": "master",
+			"majors": [ "law" ],
+			"dates": 2009,
+			"url": "http://www.flaw.uniba.sk/index.php?id=1"
+		},
+		{
+			"name" : "Faculty of Mathematics",
+			"location" : "Bratislava, Slovakia",
+			"degree": "bachelor (unfinished)",
+			"majors": [ "financial mathematics" ],
+			"dates": 2008,
+			"url": "http://www.fmph.uniba.sk/"
+		} 		
+	],
+	"onlineCourses": [
+		{
+			"title": "Front-End Developer",
+		    "school": "Udacity",
+			"date": 2015,
+	        "url": "https://www.udacity.com/"			
+		},		
+		{
+			"title": "JQuery introduction",
+		    "school": "Lynda.com",
+			"date": 2014,
+	        "url": "http://www.lynda.com"			
+		}
+	],
+	"display": function() {
+	  
+	  for (var school in education.schools) {
+	  	
+	  		 $("#education").append(HTMLschoolStart);
+	  		 $(".education-entry:last").append(
+											  HTMLschoolName.replace( "%data%" , education.schools[school].name ) + 
+											  HTMLschoolDegree.replace( "%data%" , education.schools[school].degree ))
+									  .append(HTMLschoolDates.replace( "%data%" , education.schools[school].dates ))
+									  .append(HTMLschoolLocation.replace( "%data%" , education.schools[school].location ))
+									  .append(HTMLschoolMajor.replace( "%data%" , education.schools[school].majors[0] ));
+		}
+		
+	 $("#education").append(HTMLonlineClasses);
+	  for (var course in education.onlineCourses) {
+	  		 $("#education").append(HTMLschoolStart);
+	  		 $(".education-entry:last").append(
+							  		 			HTMLonlineTitle.replace( "%data%" , education.onlineCourses[course].title ) + 
+							  		 			HTMLonlineSchool.replace( "%data%" , education.onlineCourses[course].school ))
+										.append(HTMLonlineDates.replace( "%data%" , education.onlineCourses[course].date ))
+										.append(HTMLonlineURL.replace( "%data%" , education.onlineCourses[course].url ));								  
+		}		  
+	}	
+}
+
+
+work = {
+	"jobs" : [
+		{
+		"employer" : "AT&T",
+		"title" : "web developer",
+		"location" : "Bratislava, Slovakia",
+		"dates" : "2010 - now",
+		"description": "I am doing development of internal corporate tools."
+		},
+		{
+		"employer" : "Salans",
+		"title" : "legal clerk",
+		"location" : "Bratislava, Slovakia",
+		"dates" : "2007 - 2009",
+		"description": "I was doing legal analysis, translation and communication with state auhorities."	
+		},
+		{
+		"employer" : "Forno Vivo",
+		"title" : "kitchen porter",
+		"location" : "Tring, UK",
+		"dates" : "Jun 2005 - Sept 2005",
+		"description": "doing washing-up, cleaning and other minor works."	
+		}
+	  ],
+	 "display" : function() {
+		
+		  for (var job in work.jobs) {
+		  		 $("#workExperience").append(HTMLworkStart);
+		  		 $(".work-entry:last").append(
+							  		 			HTMLworkEmployer.replace( "%data%" , work.jobs[job].employer) + 
+							  		 			HTMLworkTitle.replace( "%data%" , work.jobs[job].title ))
+									  .append(HTMLworkLocation.replace( "%data%" , work.jobs[job].location ))
+									  .append(HTMLworkDates.replace( "%data%" , work.jobs[job].dates ))
+									  .append(HTMLworkDescription.replace( "%data%" , work.jobs[job].description ));
+			}
+		}
+	}	
+			
+project = {
+	"projects" : [
+		{
+		"title" : "Udacity Mockup to Website",		
+		"dates" : "February 14th, 2015",		
+		"description" : "I was provided with a design mockup as a PDF-file and must have replicated that design in HTML and CSS. It was developed in a responsive website that displays images, descriptions and links to each of the portfolio projects.",					
+		"images" : [
+			"images/Proj1_01.png"		
+		]
+		},
+		{
+		"title" : "Udacity Interactive Resume",		
+		"dates" : "February 20th 2015",		
+		"description" : "I demonstrated mastery of the language's syntax through a series of challenges. Each multipart problem mimics a real-life challenge that front-end developers face. I was required to write clean code and to apply your knowledge of variables, objects, JSON, functions and control flow to successfully solve the challenges.",					
+		"images" : [
+			"images/Proj2_01.png",
+			"images/Proj2_02.png"		
+		]
+		}
+	],
+	"display": function() {
+		  
+		  for (var item in project.projects) {
+		  		 $("#projects").append(HTMLprojectStart);
+		  		 $(".project-entry:last").append(HTMLprojectTitle.replace( "%data%" , project.projects[item].title))
+			    						  .append(HTMLprojectDates.replace( "%data%" , project.projects[item].dates ))
+				    					  .append(HTMLprojectDescription.replace( "%data%" , project.projects[item].description ));
+			
+					for (var pic = 0; pic <  project.projects[item].images.length; pic++) {
+						$(".project-entry:last").append(HTMLprojectImage.replace( "%data%" , project.projects[item].images[pic] ));
+					}						  										  
+			}	
+	}
+}
+	
+// calling of methods
+bio.display();
+education.display();
+work.display();	
+project.display();	
+
+$("#main").append(internationalizeButton);
+$("#mapDiv").append(googleMap);
+	
+	
